ab9278a8f74b2e7a95260bc5de5c14cfe8417bb0
Angus <ajdfajdfl2003@gmail.com>
2017-01-09 21:19:18 +0800

    Feat: Add Production Code

--- TDDGroupSummaryCalculator/GroupSummaryCalculator.cs
+++ TDDGroupSummaryCalculator/GroupSummaryCalculator.cs
@@ -1,6 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
 namespace TDD
 {
     public class  GroupSummaryCalculator<T>
     {
+        private IEnumerable<T> _source;
+
+        public GroupSummaryCalculator(IEnumerable<T> source) {
+            this._source = source;
+        }
+        public IEnumerable<int> CheckSummary(int groupSize, Func<T,int> column) {
+            if(groupSize < 0) {
+                throw new ArgumentOutOfRangeException("GroupingSize should be greater than or equal to zero.");
+            }
+            for(int index = 0; index<_source.Count(); index+=groupSize) {
+                yield return _source.Skip(index).Take(groupSize).Sum(column);
+            }
+        }
     }
 }
